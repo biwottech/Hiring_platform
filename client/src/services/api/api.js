75512185d51e1e55d5bec3fe073128d05261b5b7
biwottech <biwottech@gmail.com>
2024-12-05 11:38:17 +0300

    Update

--- client/src/services/api/api.js
+++ client/src/services/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:3000/api/";
+const API_BASE_URL = "http://localhost/api/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
