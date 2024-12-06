3e55f6b52ac10d29c1b4fbbdf5c0e4a4d739316f
biwottech <biwottech@gmail.com>
2024-12-06 07:15:12 +0300

    update

--- client/src/services/api/api.js
+++ client/src/services/api/api.js
@@ -5,7 +5,7 @@ const API_BASE_URL = "http://localhost:3000/api/";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json", 
   },
 });
 
